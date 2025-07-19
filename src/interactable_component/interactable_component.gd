0655affd9bf9d47e63fcf3ceb4d3592feca9d15b
sebashtioon <sebastiansuciu607@gmail.com>
2025-07-19 09:26:39 +0300

    yay

--- src/interactable_component/interactable_component.gd
+++ src/interactable_component/interactable_component.gd
@@ -3,9 +3,19 @@ extends Node3D
 signal action_triggered
 
 var interacting : bool = false
-
 var player_area_box_active : bool = false
 var mimic_raycast_box_active : bool = false
+
+@export var Contents_Node : Node3D
+@export var UI_Sprite_Node : Sprite3D
+@export var SubViewport_Node : SubViewport
+
+func _ready() -> void:
+	toggle_interacting(false)
+
+func toggle_interacting(interacting_val : bool):
+	Contents_Node.visible = interacting_val
+	interacting = interacting_val
 
 func _process(_delta: float) -> void:
 	if player_area_box_active and mimic_raycast_box_active:
