40f84e8264b3dedde9da42b110d20ce565f6817c
sebashtioon <sebastiansuciu607@gmail.com>
2025-07-27 17:10:36 +1000

    Add keyboard and mouse input textures
    Added a comprehensive set of keyboard and mouse input textures and their outline variants to ast/textures/input/. These assets will be used for input visualization and UI elements. Also updated drag_interaction and main_level scenes and scripts to support or reference these new textures.

--- src/main_level/main_level.gd
+++ src/main_level/main_level.gd
@@ -1,4 +1,4 @@
 extends Node3D
 
 func _physics_process(_delta: float) -> void:
-	$MovingBody/DragInteraction.target_position = PlayerGlobal.drag_interaction_player_position
+	$Pizza/DragInteraction.target_position = PlayerGlobal.drag_interaction_player_position
