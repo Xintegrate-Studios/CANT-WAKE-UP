338faeaa646b92eef7df0065cc0d630f5c6fe5cb
sebashtioon <sebastiansuciu607@gmail.com>
2025-07-28 20:38:14 +1000

    Clear dragging bodies when dragging ends
    Added a setter to the 'currently_dragging' variable to automatically clear the 'currently_dragging_bodies' array when dragging is set to false. This ensures that the list of dragging bodies is reset appropriately.

--- src/globals/draggable_bodies_global.gd
+++ src/globals/draggable_bodies_global.gd
@@ -2,5 +2,8 @@ extends Node
 
 var currently_hovering_over : bool = false
 var currently_hovering_over_body
-var currently_dragging : bool = false
-var currently_dragging_bodies : Array = []
+var currently_dragging : bool = false:
+	set(value):
+		currently_dragging = value
+		if !value: currently_dragging_bodies.clear()
+var currently_dragging_bodies = []
