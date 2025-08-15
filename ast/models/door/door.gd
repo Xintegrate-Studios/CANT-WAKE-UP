a7bcfbf57bdd988052f2418704552420365ac168
sebashtioon <sebastiansuciu607@gmail.com>
2025-08-15 21:11:23 +1000

    Add door node with state and animation structure
    Introduces a new door node to the main level, including a Door script with state management (open/closed), an AnimationPlayer, and a HingePivot for future animation support. Updates the scene hierarchy and connections to accommodate the new structure.

--- ast/models/door/door.gd
+++ ast/models/door/door.gd
@@ -1,1 +1,9 @@
 extends Node3D
+
+enum DOOR_STATES {CLOSED, OPEN}
+
+var DOOR_STATE : DOOR_STATES
+@export var STARTING_DOOR_STATE : DOOR_STATES = DOOR_STATES.CLOSED
+
+func _ready() -> void:
+	pass
