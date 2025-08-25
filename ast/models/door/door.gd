84494d411b5a11a17f80cc3e620cc95c09e3a6f5
sebashtioon <sebastiansuciu607@gmail.com>
2025-08-25 17:02:35 +1000

    fix door glitch and enlarge roof

--- ast/models/door/door.gd
+++ ast/models/door/door.gd
@@ -10,15 +10,15 @@ var DOOR_STATE : DOOR_STATES
 
 func _ready() -> void:
 	DOOR_STATE = STARTING_DOOR_STATE
-	if DOOR_STATE == DOOR_STATES.OPEN:
+	if DOOR_STATE == DOOR_STATES.CLOSED:
 		HingePivot.rotation_degrees.y = 90
 	else:
 		HingePivot.rotation_degrees.y = 0
 
 func _door_triggered() -> void:
-	if DOOR_STATE == DOOR_STATES.OPEN:
-		DOOR_STATE = DOOR_STATES.CLOSED
+	if DOOR_STATE == DOOR_STATES.CLOSED:
+		DOOR_STATE = DOOR_STATES.OPEN
 		DoorAnimation.play(&"main", -1, -1, true)
 	else:
-		DOOR_STATE = DOOR_STATES.OPEN
+		DOOR_STATE = DOOR_STATES.CLOSED
 		DoorAnimation.play(&"main")
