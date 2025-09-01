e6e198bb236292792a2aebe3a18ce45e978bf142
sebashtioon <sebastiansuciu607@gmail.com>
2025-09-01 18:42:56 +1000

    hoy vulgar

--- src/globals/tasks_global.gd
+++ src/globals/tasks_global.gd
@@ -1,10 +1,43 @@
 extends Node
 
 enum TASKS {
+
+    # Normal tasks
 	GO_TO_BED,
 	FOLD_CLOTHES,
 	UNPACK_BOXES,
 	TURN_OFF_LIGHTS,
 	MAKE_DINNER,
 	TAKE_SHOWER,
+    BRUSH_TEETH,
+    PUT_AWAY_TOYS,
+    WATER_PLANTS,
+    WASH_DISHES,
+
+    # Late game disturbing tasks
+    KILLING_YOURSELF,
+    STOP_BREATHING,
+    HOLD_BREATH_FOREVER,
+    CUT_THROAT,
 }
+
+
+const TASKS_2 = [
+    ["GO_TO_BED", "Go to bed"],
+    ["FOLD_CLOTHES", "Fold your clothes"],
+    ["UNPACK_BOXES", "Unpack the storage boxes"],
+    ["TURN_OFF_LIGHTS", "Turn off the lights"],
+    ["MAKE_DINNER", "Make dinner"],
+    ["TAKE_SHOWER", "Take a shower"],
+    ["BRUSH_TEETH", "Brush your teeth"],
+    ["PUT_AWAY_TOYS", "Put away your toys"],
+    ["WATER_PLANTS", "Water the plants"],
+    ["WASH_DISHES", "Wash the dishes"],
+
+    ["KILLING_YOURSELF", "Kill yourself"],
+    ["STOP_BREATHING", "Stop breathing"],
+
+]
+
+##    ["HOLD_BREATH_FOREVER", "Hold your fucking breath forever"],
+##    ["CUT_THROAT", "Cut your throat cunt"],
