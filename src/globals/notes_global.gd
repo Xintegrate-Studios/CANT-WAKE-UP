cc30d053f3fe698104066a704282c6c599d5d4ef
sebashtioon <sebastiansuciu607@gmail.com>
2025-10-26 12:24:47 +1100

    Update note text in notes_global.gd
    Replaced the empty note for key 1 with a detailed message from 'mom & dad' and added a placeholder for key 2. This provides more meaningful content for the notes system.

--- src/globals/notes_global.gd
+++ src/globals/notes_global.gd
@@ -1,6 +1,28 @@
 extends Node
 
 const NOTES : Dictionary = {
-	1 : "",
-	
+	1 : "hey kiddo,
+
+mom and dad want you to get a few 
+things done before we’re back. we’ll 
+be home late tonight, so please go to 
+bed early, okay?
+
+- fold your clothes
+- unpack your storage box
+- put away your toys
+- make yourself dinner. there’s some 
+pizza in the fridge
+- wash the dishes
+- brush your teeth
+
+we might not wake you when we get in,
+so don’t worry if you hear us moving around.
+just stay in bed.
+love you.
+
+— mom & dad",
+
+
+	2: "placeholder"
 }
