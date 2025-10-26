666d3938be9fbd0759075f409cd22d9679b0095c
sebashtioon <sebastiansuciu607@gmail.com>
2025-10-26 12:31:31 +1100

    Refactor note content handling and add interactable component
    Moved note text from player and table note scenes to a dynamic source, replacing hardcoded text with a placeholder. Updated table_note.gd to set note content from NotesGlobal and added an interactable component to table_note.tscn for interaction logic.

--- ast/actors/table note/table_note.gd
+++ ast/actors/table note/table_note.gd
@@ -2,5 +2,11 @@ extends Node3D
 
 var NOTE_ID : int = 1 # default note id to show
 
-func _update_note() -> void:
-	pass
+@export var NoteContent : Label3D
+
+func _update_note(note_id : int) -> void:
+	NoteContent.text = NotesGlobal.NOTES[note_id]
+
+
+func _on_note_closeup() -> void:
+	pass # Replace with function body.
