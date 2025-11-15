72740c957e9648552ba87c931fec51c42ccb825a
sebashtioon <sebastiansuciu607@gmail.com>
2025-11-15 16:36:49 +1100

    Add wardrobe hiding detection and improve collision
    Introduces an area to detect when the player is inside the wardrobe, updating PlayerGlobal.in_wardrobe accordingly. Refactors wardrobe collision shapes to use BoxShape3D for better performance and accuracy, and manages collision process modes based on wardrobe state.

--- ast/actors/wardrobe/wardrobe.gd
+++ ast/actors/wardrobe/wardrobe.gd
@@ -4,9 +4,16 @@ var open : bool = false:
 	set(value):
 		open = value
 		
+		
 		open_mesh.visible = open
 		closed_mesh.visible = !open
 		
+		if open:
+			open_collision.process_mode = Node.PROCESS_MODE_INHERIT
+			closed_collision.process_mode = Node.PROCESS_MODE_DISABLED
+		else:
+			open_collision.process_mode = Node.PROCESS_MODE_DISABLED
+			closed_collision.process_mode = Node.PROCESS_MODE_INHERIT
 
 @export var open_mesh : MeshInstance3D
 @export var closed_mesh : MeshInstance3D
@@ -15,3 +22,11 @@ var open : bool = false:
 
 func _on_wardrobe_toggled() -> void:
 	open = !open
+
+func _on_in_wardrobe_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group(&"PlayerBody"):
+		PlayerGlobal.in_wardrobe = true
+
+func _on_in_wardrobe_area_body_exited(body: Node3D) -> void:
+	if body.is_in_group(&"PlayerBody"):
+		PlayerGlobal.in_wardrobe = false
--- src/globals/player_global.gd
+++ src/globals/player_global.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var drag_interaction_player_position : Vector3
+var in_wardrobe : bool = false
 
 var NIGHT_IDX : int = 1 # default to night 1
 
