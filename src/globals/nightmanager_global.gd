251eb0042ba9068b98c9f82e45b6ccc85f23ecec
sebashtioon <sebastiansuciu607@gmail.com>
2025-11-22 07:29:26 +1100

    Move NIGHT_IDX variable to nightmanager_global.gd
    Relocated the NIGHT_IDX variable from player_global.gd to nightmanager_global.gd to better reflect its purpose and improve code organization.

--- src/globals/nightmanager_global.gd
+++ src/globals/nightmanager_global.gd
@@ -1,1 +1,3 @@
 extends Node
+
+var NIGHT_IDX : int = 1 # default to night 1
--- src/globals/player_global.gd
+++ src/globals/player_global.gd
@@ -3,8 +3,6 @@ extends Node
 var drag_interaction_player_position : Vector3
 var in_wardrobe : bool = false
 
-var NIGHT_IDX : int = 1 # default to night 1
-
 var in_ui : bool = false
 
 var player : CharacterBody3D
