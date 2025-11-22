04a4d8036a6365441d82bb8e95d5fe3b5a14cd7e
sebashtioon <sebastiansuciu607@gmail.com>
2025-11-23 07:44:28 +1100

    Add open/close functionality to cardboard box
    Implemented logic to toggle between open and closed states for the cardboard box actor, including mesh visibility and collision adjustments. Updated scene and physics layers to support new interactions and refined transforms for better placement and UI scaling.

--- ast/actors/cardboard box/cardboard_box.gd
+++ ast/actors/cardboard box/cardboard_box.gd
@@ -1,5 +1,16 @@
 extends Node3D
 
+var open : bool = false:
+	set(value):
+		open = value
+		open_mesh.visible = open
+		closed_mesh.visible = !open
+
+@export var open_mesh : MeshInstance3D
+@export var closed_mesh : MeshInstance3D
+
+func _ready() -> void:
+	open = false
 
 func _on_interactable_component_action_triggered() -> void:
-	pass # Replace with function body.
+	open = !open
