8983de36340b053013553487f289eeba1b573378
sebashtioon <sebastiansuciu607@gmail.com>
2026-01-09 09:05:21 +1100

    Add blinking phase tracking to BlinkingMechanic
    Introduced a BLINKING_PHASE variable to track the current phase of the blinking animation. Added signal connections and handlers for animation start and finish events to update the phase accordingly. Also updated the main level scene to display the current blink phase in the debug HUD.

--- src/mechanics/blinking_mechanic.gd
+++ src/mechanics/blinking_mechanic.gd
@@ -1,5 +1,13 @@
 extends Node3D
 
+# phases:
+# OPEN
+# CLOSING
+# CLOSED
+# OPENING
+
+var BLINKING_PHASE : String = "OPEN"
+
 func _input(_event: InputEvent) -> void:
 	if PlayerGlobal.sleeping:
 		if Input.is_action_just_pressed("Blink"):
@@ -7,3 +15,18 @@ func _input(_event: InputEvent) -> void:
 		
 		if Input.is_action_just_released("Blink"):
 			PlayerGlobal.world.blink_animations.play(&"open")
+
+
+# blinking phase handling
+
+func _on_blink_animations_animation_finished(anim_name: StringName) -> void:
+	if anim_name == &"open":
+		BLINKING_PHASE = "OPEN"
+	if anim_name == &"close":
+		BLINKING_PHASE = "CLOSED"
+
+func _on_blink_animations_animation_started(anim_name: StringName) -> void:
+	if anim_name == &"open":
+		BLINKING_PHASE = "OPENING"
+	if anim_name == &"close":
+		BLINKING_PHASE = "CLOSING"
