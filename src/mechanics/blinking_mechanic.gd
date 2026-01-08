9121538e9002ad3ec56e7ae8e8395c463800db7f
sebashtioon <sebastiansuciu607@gmail.com>
2026-01-09 09:45:42 +1100

    Add demon eyes and blink phase label to main level
    Added two red emissive eyes to the demon model in the main level scene. Introduced a new 'blink_phase' label to the debug HUD and updated the blinking mechanic script to display the current blink phase in real time. The demon is now shown when the blink animation closes.

--- src/mechanics/blinking_mechanic.gd
+++ src/mechanics/blinking_mechanic.gd
@@ -8,6 +8,11 @@ extends Node3D
 
 var BLINKING_PHASE : String = "OPEN"
 
+@export var blink_phase_lbl : Label
+
+func _process(_delta: float) -> void:
+	blink_phase_lbl.text = "BLINK PHASE: " + BLINKING_PHASE
+
 func _input(_event: InputEvent) -> void:
 	if PlayerGlobal.sleeping:
 		if Input.is_action_just_pressed("Blink"):
@@ -23,6 +28,7 @@ func _on_blink_animations_animation_finished(anim_name: StringName) -> void:
 	if anim_name == &"open":
 		BLINKING_PHASE = "OPEN"
 	if anim_name == &"close":
+		$"../demon".show()
 		BLINKING_PHASE = "CLOSED"
 
 func _on_blink_animations_animation_started(anim_name: StringName) -> void:
