fbf8a42d482f48e74e625e3bca1728f12ed4717d
sebashtioon <sebastiansuciu607@gmail.com>
2026-01-09 08:57:07 +1100

    BLINKING

--- src/mechanics/blinking_mechanic.gd
+++ src/mechanics/blinking_mechanic.gd
@@ -3,7 +3,7 @@ extends Node3D
 func _input(_event: InputEvent) -> void:
 	if PlayerGlobal.sleeping:
 		if Input.is_action_just_pressed("Blink"):
-			pass
+			PlayerGlobal.world.blink_animations.play(&"close")
 		
 		if Input.is_action_just_released("Blink"):
-			pass
+			PlayerGlobal.world.blink_animations.play(&"open")
