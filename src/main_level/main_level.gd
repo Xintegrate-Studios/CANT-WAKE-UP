81da163471d069f4711f3224bfdd64a5a9a954e6
sebashtioon <sebastiansuciu607@gmail.com>
2026-01-13 08:12:46 +1100

    Reduce mouse look sensitivity
    Halved the sensitivity for mouse look by changing the rotation multiplier from 0.0001 to 0.00005 for both X and Y axes. This should make camera movement smoother and less abrupt.

--- src/main_level/main_level.gd
+++ src/main_level/main_level.gd
@@ -14,8 +14,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		_handle_mouse_look(event.relative)
 
 func _handle_mouse_look(mouse_relative: Vector2) -> void:
-	sleep_head.rotate_y(-mouse_relative.x * 0.0001)
-	sleep_camera.rotate_x(-mouse_relative.y * 0.0001)
+	sleep_head.rotate_y(-mouse_relative.x * 0.00005)
+	sleep_camera.rotate_x(-mouse_relative.y * 0.00005)
 	sleep_camera.rotation.x = clamp(sleep_camera.rotation.x, deg_to_rad(-40), deg_to_rad(40))
 	sleep_head.rotation.y = clamp(sleep_head.rotation.y, deg_to_rad(-130), deg_to_rad(0))
 	
