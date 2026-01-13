0d62aed8ce489502cfb16a51e99b0ad6194f2ccf
sebashtioon <sebastiansuciu607@gmail.com>
2026-01-13 21:15:45 +1100

    Enhance phase management by adding soft and hard maximum phases for each night

--- src/globals/paralysisphasemanager_global.gd
+++ src/globals/paralysisphasemanager_global.gd
@@ -4,16 +4,37 @@ enum ParalysisPhase { NORMAL, ENCOUNTER, REALITYDISTORTION }
 const PHASE_COUNT : int = int(ParalysisPhase.REALITYDISTORTION) + 1
 var paralysis_phase : ParalysisPhase = ParalysisPhase.NORMAL
 
+# as a percentage
+var chance_to_wake_up_on_soft_max_by_night : Dictionary = {
+	1: 100.0,
+	2: 90.0,
+	3: 85.0,
+}
+
+# absolute minimum phases for each night (night 1 is always hard locked at 3 phases)
 var min_phases_by_night : Dictionary = {
-	1: 3
+	1: 3,
+	2: 3,
 }
 
+# minimum max phases for each night, after this phase count the
+# chance_to_wake_up_on_soft_max_by_night can be used to determine
+# whether the player stays or wakes up, every time the player switches phases.
+# if the chance is a success, the player wakes up. if not, the player
+# stays in the phase cycle until the hard max phase threshold is hit (below)
 var soft_max_phases_by_night : Dictionary = {
-	1: 4
+	1: 4,
+	2: 4,
 }
 
+# absolute max phases for each night. the player CAN NOT have any more phases in a night
+# than the value here. for example, if the player is on phase 4 (whatever it may be, normal,
+# an encounter or a reality distortion, and the hard max is 5 phases in that night,
+# and they complete the phase, they must complete 1 more (5 phases total) before waking up,
+# but that assumes the chance to wake up on the soft max wasnt successfull.
 var hard_max_phases_by_night : Dictionary = {
-	1: 3
+	1: 3,
+	2: 5,
 }
 
 # [min, max] sec
@@ -22,6 +43,7 @@ var phase_length_bounds_by_night : Dictionary = {
 	# TODO: ADD MORE
 }
 
+# adjust each to whatever, they are ratios anyway
 var phase_weights_by_night : Dictionary = {
 	1: [1, 1, 1],
 	2: [1, 1, 1],
