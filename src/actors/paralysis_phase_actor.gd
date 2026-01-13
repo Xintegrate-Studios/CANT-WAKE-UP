09c360968aaa4aa9f28bb157d90490c6c8888f45
sebashtioon <sebastiansuciu607@gmail.com>
2026-01-13 13:39:21 +1100

    Add phase switch countdown initialization and timeout handling

--- src/actors/paralysis_phase_actor.gd
+++ src/actors/paralysis_phase_actor.gd
@@ -1,5 +1,11 @@
 extends Node3D
 
+@export var phase_switch_countdown : Timer
+
+func init_switch_countdown(night_idx : int):
+	phase_switch_countdown.wait_time
+	phase_switch_countdown.start()
+
 func START_PHASES() -> void:
 	if NightmanagerGlobal.NIGHT_IDX == 1:
 		ParalysisphasemanagerGlobal.SWAP_PHASE(false, ParalysisphasemanagerGlobal.ParalysisPhase.NORMAL)
@@ -11,7 +17,12 @@ func START_PHASES() -> void:
 
 func _on_phase_switch_countdown_timeout() -> void:
 	if NightmanagerGlobal.NIGHT_IDX == 1:
-		if ParalysisphasemanagerGlobal.paralysis_phase == ParalysisphasemanagerGlobal.ParalysisPhase.ENCOUNTER:
-			ParalysisphasemanagerGlobal.SWAP_PHASE()
+		if ParalysisphasemanagerGlobal.paralysis_phase != ParalysisphasemanagerGlobal.ParalysisPhase.REALITYDISTORTION:
+			if ParalysisphasemanagerGlobal.paralysis_phase == ParalysisphasemanagerGlobal.ParalysisPhase.ENCOUNTER:
+				ParalysisphasemanagerGlobal.SWAP_PHASE(false, ParalysisphasemanagerGlobal.ParalysisPhase.REALITYDISTORTION)
+				print("now on phase: " + str(ParalysisphasemanagerGlobal.paralysis_phase))
+			else:
+				ParalysisphasemanagerGlobal.SWAP_PHASE(false, ParalysisphasemanagerGlobal.ParalysisPhase.ENCOUNTER)
+				print("now on phase: " + str(ParalysisphasemanagerGlobal.paralysis_phase))
 		else:
-			pass
+			print("DONE")
