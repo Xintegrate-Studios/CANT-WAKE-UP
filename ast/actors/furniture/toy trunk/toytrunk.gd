3015e4f3e68b80f77f4696d2d37274ed960510cc
sebashtioon <sebastiansuciu607@gmail.com>
2026-01-17 22:23:24 +1100

    Add toy trunk actor with interaction logic
    Introduces a new toy trunk actor with a GDScript for handling open/close state, draggable items, and task completion. Updates the scene to include the trunk, its mesh, lid, interactable component, and relevant collision and UI nodes.

---
+++ ast/actors/furniture/toy trunk/toytrunk.gd
@@ -0,0 +1,68 @@
+extends Node3D
+
+var draggables_inside : Array[DraggableBody] = []
+var open : bool = false:
+	set(value):
+		open = value
+		
+		# if in wardrobe and its closed, stop dragging objects
+		if !open:
+			DraggableBodiesGlobal.let_go_of_all_bodies()
+		
+		if open:
+			pass
+			#lid.rotation_degrees = Vector3.ONE
+		else:
+			pass
+		
+		for draggable in draggables_inside:
+			draggable.interaction_disabled = !open
+			draggable.let_go()
+			draggable.hovering_over = false
+		
+		if open:
+			open_collision.process_mode = Node.PROCESS_MODE_INHERIT
+			open_draggable_collision.process_mode = Node.PROCESS_MODE_INHERIT
+			closed_collision.process_mode = Node.PROCESS_MODE_DISABLED
+		else:
+			open_collision.process_mode = Node.PROCESS_MODE_DISABLED
+			open_draggable_collision.process_mode = Node.PROCESS_MODE_DISABLED
+			closed_collision.process_mode = Node.PROCESS_MODE_INHERIT
+
+var clothes_inside : bool = false:
+	set(value):
+		clothes_inside = value
+		# TASKS
+		if value:
+			TasksGlobal.complete_task("CLOTHES_AWAY")
+		else:
+			TasksGlobal.uncomplete_task("CLOTHES_AWAY")
+
+@export var lid : MeshInstance3D
+@export var open_collision : StaticBody3D
+@export var open_draggable_collision : StaticBody3D
+@export var closed_collision : StaticBody3D
+
+func _ready() -> void:
+	open = false
+
+func _on_trunk_toggled() -> void:
+	open = !open
+
+func _on_in_wardrobe_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group(&"DraggableBody"):
+		draggables_inside.append(body)
+		if body.ID == DraggableBodiesGlobal.BODY_IDS.CLOTHES_PILE:
+			print("--clothes inside wardrobe")
+			clothes_inside = true
+
+func _on_in_wardrobe_area_body_exited(body: Node3D) -> void:
+	if body.is_in_group(&"DraggableBody"):
+		draggables_inside.erase(body)
+		if body.ID == DraggableBodiesGlobal.BODY_IDS.CLOTHES_PILE:
+			print("!--clothes outside wardrobe")
+			clothes_inside = false
+
+
+func _on_in_wardrobe_bug_debounce_timeout() -> void:
+	pass # Replace with function body.
