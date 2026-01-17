315ffda593c5dd7e38d75fe34aab86240084d99f
sebashtioon <sebastiansuciu607@gmail.com>
2026-01-17 22:32:00 +1100

    Refactor toy trunk lid logic and update scene setup
    Updated the toy trunk script to directly set the lid's rotation when opening or closing, and commented out collision process mode changes. Adjusted the scene file to reference the lid node, modified transforms, updated collision shapes, and fixed UI viewport texture paths for better consistency and functionality.

--- ast/actors/furniture/toy trunk/toytrunk.gd
+++ ast/actors/furniture/toy trunk/toytrunk.gd
@@ -5,29 +5,27 @@ var open : bool = false:
 	set(value):
 		open = value
 		
-		# if in wardrobe and its closed, stop dragging objects
 		if !open:
 			DraggableBodiesGlobal.let_go_of_all_bodies()
 		
 		if open:
-			pass
-			#lid.rotation_degrees = Vector3.ONE
+			lid.rotation_degrees.z = 63.0
 		else:
-			pass
+			lid.rotation_degrees.z = 0.0
 		
 		for draggable in draggables_inside:
 			draggable.interaction_disabled = !open
 			draggable.let_go()
 			draggable.hovering_over = false
 		
-		if open:
-			open_collision.process_mode = Node.PROCESS_MODE_INHERIT
-			open_draggable_collision.process_mode = Node.PROCESS_MODE_INHERIT
-			closed_collision.process_mode = Node.PROCESS_MODE_DISABLED
-		else:
-			open_collision.process_mode = Node.PROCESS_MODE_DISABLED
-			open_draggable_collision.process_mode = Node.PROCESS_MODE_DISABLED
-			closed_collision.process_mode = Node.PROCESS_MODE_INHERIT
+		#if open:
+			#open_collision.process_mode = Node.PROCESS_MODE_INHERIT
+			#open_draggable_collision.process_mode = Node.PROCESS_MODE_INHERIT
+			#closed_collision.process_mode = Node.PROCESS_MODE_DISABLED
+		#else:
+			#open_collision.process_mode = Node.PROCESS_MODE_DISABLED
+			#open_draggable_collision.process_mode = Node.PROCESS_MODE_DISABLED
+			#closed_collision.process_mode = Node.PROCESS_MODE_INHERIT
 
 var clothes_inside : bool = false:
 	set(value):
@@ -52,16 +50,10 @@ func _on_trunk_toggled() -> void:
 func _on_in_wardrobe_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group(&"DraggableBody"):
 		draggables_inside.append(body)
-		if body.ID == DraggableBodiesGlobal.BODY_IDS.CLOTHES_PILE:
-			print("--clothes inside wardrobe")
-			clothes_inside = true
 
 func _on_in_wardrobe_area_body_exited(body: Node3D) -> void:
 	if body.is_in_group(&"DraggableBody"):
 		draggables_inside.erase(body)
-		if body.ID == DraggableBodiesGlobal.BODY_IDS.CLOTHES_PILE:
-			print("!--clothes outside wardrobe")
-			clothes_inside = false
 
 
 func _on_in_wardrobe_bug_debounce_timeout() -> void:
